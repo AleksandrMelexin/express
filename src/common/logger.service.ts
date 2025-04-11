04c5a3919ad407d1a73a7735674d3fa3402554c5
AleksandrMelexin <Melehin-Sasha7@yandex.ru>
2025-04-11 18:53:18 +0300

    Переделана конкатенация строк в логгере

--- src/common/logger.service.ts
+++ src/common/logger.service.ts
@@ -1,21 +1,21 @@
 export class LoggerService {
 	constructor() {
-        this.successLog("LoggerService запущен");
+        this.successLog(`LoggerService запущен`);
 	}
 
     successLog(message: string) {
-        console.log("[success] "+message+" :)");
+        console.log(`[success] ${message} :)`);
     }
 
     infoLog(message: string) {
-        console.log("[info] "+message);
+        console.log(`[info] ${message}`);
     }
 
     warningLog(message: string) {
-        console.log("[warning] "+message);
+        console.log(`[warning] ${message}`);
     }
 
     errorLog(message: string) {
-        console.log("[error] "+message+" :(");
+        console.log(`[error] ${message} :(`);
     }
 }
